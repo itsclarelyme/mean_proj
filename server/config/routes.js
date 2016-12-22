89225a49ee4215fa061204fc90d53b8eb7c15728
calvin_nguyen <imcalv@gmail.com>
2016-12-21 20:27:17 -0800

    login, reg, layout

--- server/config/routes.js
+++ server/config/routes.js
@@ -11,9 +11,12 @@ var users = require('../controllers/users.js');
 
 module.exports = function(app) {
 
+  //users
   app.get('/users', users.index);
+  app.get('/users/:id', users.index_login);
   app.post('/login', users.login);
   app.post('/register', users.register);
+  app.post('/profile', users.add_profile);
 
 }
 
